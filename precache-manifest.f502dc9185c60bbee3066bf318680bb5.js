9201c88e93a0810c50ad86c1274e4ceb4a721748
Dmitri <dsharsh@hotmail.com>
2019-02-17 09:43:16 -0700

    Updates

---
+++ precache-manifest.f502dc9185c60bbee3066bf318680bb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7b90fb4a25440fafd09",
+    "url": "/robohomies/static/js/runtime~main.c7b90fb4.js"
+  },
+  {
+    "revision": "827cb9e08c7756a2c8ef",
+    "url": "/robohomies/static/js/main.827cb9e0.chunk.js"
+  },
+  {
+    "revision": "9e6fe66e62d4b911ec56",
+    "url": "/robohomies/static/js/1.9e6fe66e.chunk.js"
+  },
+  {
+    "revision": "827cb9e08c7756a2c8ef",
+    "url": "/robohomies/static/css/main.7cf0ff69.chunk.css"
+  },
+  {
+    "revision": "13a2f58d07705c3cb680acabbf27d9d9",
+    "url": "/robohomies/index.html"
+  }
+];
